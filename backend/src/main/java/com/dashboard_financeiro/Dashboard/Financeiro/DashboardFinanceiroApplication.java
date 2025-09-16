0a58f2bbf2bffa25ac35dc1352c66d3e30b6e2eb
= <=>
2025-09-16 19:47:37 -0300

    first commit

---
+++ backend/src/main/java/com/dashboard_financeiro/Dashboard/Financeiro/DashboardFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard_financeiro.Dashboard.Financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardFinanceiroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dashboard_financeiro/Dashboard/Financeiro/DashboardFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard_financeiro.Dashboard.Financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
